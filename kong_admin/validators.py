0b08ceae146533248c316e36477889e7016c3836
Dirk Moors <dirkmoors@gmail.com>
2015-09-29 16:38:54 +0200

    Fixed a pep8 issue

--- kong_admin/validators.py
+++ kong_admin/validators.py
@@ -5,7 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 from django.core import validators as django_validators
 
 
-name_validator = django_validators.RegexValidator(r'^[\w.~-]+$',
-      _('Enter a valid username. '
-        'This value may contain only letters, numbers '
-        'and ~/./-/_ characters.'), 'invalid')
+name_validator = django_validators.RegexValidator(
+    r'^[\w.~-]+$', _('Enter a valid username. This value may contain only letters, '
+                     'numbers and ~/./-/_ characters.'), 'invalid')

4ef65833326c32183b2b9fc2ca55ed8ff782caee
Dirk Moors <dirkmoors@gmail.com>
2015-10-13 11:44:50 +0200

    Updated receiver import

--- kong_admin/apps.py
+++ kong_admin/apps.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 from django.apps import AppConfig
-from kong_admin.receivers import *
 
 
 class KongAdminConfig(AppConfig):
     name = 'kong_admin'
     verbose_name = 'Kong'
+
+    def ready(self):
+        # See: http://stackoverflow.com/a/22924754/591217
+        from kong_admin import receivers
